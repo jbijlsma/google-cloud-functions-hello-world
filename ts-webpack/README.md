b504ff2155381f0817a13be3514108ca954b3f7e
jbijlsma <jeroen_bijlsma@yahoo.com>
2022-09-30 06:22:06 +0800

    Add webpack example

---
+++ ts-webpack/README.md
@@ -0,0 +1,19 @@
+# Intro
+
+# Local testing
+
+```
+npm start
+```
+
+# Deployment
+
+```
+npm run deploy
+```
+
+# Reading
+
+https://medium.com/google-cloud/hot-reload-node-cloud-functions-64ffdb095a00
+
+https://github.com/GoogleCloudPlatform/functions-framework-nodejs/issues/24
---
+++ ts-webpack/webpack.config.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var nodeExternals = require("webpack-node-externals");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.ts",
+  output: {
+    filename: "index.js", // <-- Important
+    libraryTarget: "this", // <-- Important
+  },
+  target: "node", // <-- Important
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true,
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  optimization: {
+    minimize: false,
+  },
+  externals: [nodeExternals()], // <-- Important
+};
